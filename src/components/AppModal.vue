70b96318594a7c3b772a612c56977b633c6f5d5b
solovev.m <msolov68@gmail.com>
2024-01-06 08:56:09 +0200

    вносит изменения в стили формы после теста на ноуте

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -30,16 +30,18 @@
 
   @media (min-width: $lg) {
     width: 594px;
-    top: 42%;
+    top: 20px;
+    transform: translateX(-50%);
 
     &__notes {
-      top: 50%;
+      top: 0;
     }
   }
 
   @media (min-width: $xlg) {
     width: 780px;
     padding: 80px;
+    top: 40px;
   }
 }
 </style>
